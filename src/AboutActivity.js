26b3bc7cb053f208bc25cbda1447fcc770511a6b
Sax <saxa@wwt.com>
2018-07-09 16:06:45 -0500

    Added a link to Wikipedia for the Mafia page. Opens the Wikipedia app if installed.

--- src/AboutActivity.js
+++ src/AboutActivity.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Linking, Text, View } from 'react-native';
 
 class AboutActivity extends React.Component {
   static navigationOptions = {
     title: 'About',
   };
 
+  constructor(props){
+    super(props);
+    this.state ={}
+  }
+
+
+  componentDidMount(){
+    {/*
+    fetch('http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=Mafia_(party_game)&callback=?')  
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+    }
+    */}
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
         <Text>The About Page</Text>
+        <Text 
+          style={{color: 'blue'}}
+          onPress={() => Linking.openURL('https://en.wikipedia.org/wiki/Mafia_(party_game)')}
+        >
+          Wikipedia
+        </Text>
       </View>
     )
   }
