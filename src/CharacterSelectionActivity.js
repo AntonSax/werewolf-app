d008e49b98790dbf18a360785e35f403d4f2f9c7
Sax <saxa@wwt.com>
2018-07-10 14:23:50 -0500

    Downloaded a Search tool to filter through a list of items. Attempted to get that to work.

--- src/CharacterSelectionActivity.js
+++ src/CharacterSelectionActivity.js
@@ -1,31 +1,71 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import SearchInput, { createFilter } from 'react-native-search-filter';
 import { Button, Card, CardSection, Header, NumberInput } from './components/common';
+import Item from './components/Item';
 import ItemCard from './components/ItemCard';
 import ItemContainer from './components/ItemContainer';
+import emails from './react-native-search-filter/mails';
+const KEYS_TO_FILTERS = ['user.name', 'subject', 'name'];
 
-export default class App extends React.Component {
+{/* https://www.npmjs.com/package/react-native-search-filter */}
+
+export default class CharacterSelectionActivity extends React.Component {
   static navigationOptions = {
     title: 'Character Selection',
   };
 
-  state = { playerCount: '' };
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      playerCount: ''
+    }
+  }
+
+  searchUpdated(term) {
+    this.setState({ searchTerm: term })
+  }
 
   render() {
     const { navigate } = this.props.navigation;
+    const filteredEmails = emails.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+    const filteredRoles = Item.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
-      <View style={{ flex: 1 }}>
-        <ItemContainer/>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <SearchInput 
+            onChangeText={(term) => { this.searchUpdated(term) }} 
+            style={styles.searchInput}
+            placeholder="Type a message to search"
+            />
+          <ScrollView>
+            {filteredEmails.map(email => {
+              return (
+                <TouchableOpacity onPress={() => alert(email.user.name)} key={email.id} style={styles.emailItem}>
+                  <View>
+                    <Text>{email.user.name}</Text>
+                    <Text style={styles.emailSubject}>{email.subject}</Text>
+                  </View>
+                </TouchableOpacity>
+              )
+            })}
+          </ScrollView>
+        </View>
 
-        <View style={styles.buttonViewStyle}>
-          <Button 
-          style={styles.buttonStyle}
-          title="Char Selection"
-          onPress={() =>
-          navigate('Lobby')
-          }>
-            Create Lobby
-          </Button>
+        <View style={{ flex: 1 }}>
+          <ItemContainer/>
+
+          <View style={styles.buttonViewStyle}>
+            <Button 
+            style={styles.buttonStyle}
+            title="Char Selection"
+            onPress={() =>
+            navigate('Lobby')
+            }>
+              Create Lobby
+            </Button>
+          </View>
         </View>
       </View>
     );
@@ -38,11 +78,29 @@ const styles = {
     marginRight: 40,
     flex: 1
   },
+  buttonViewStyle: {
+    height: 50,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start'
+  },
+  emailItem:{
+    borderBottomWidth: 0.5,
+    borderColor: 'rgba(0,0,0,0.3)',
+    padding: 10
+  },
+  emailSubject: {
+    color: 'rgba(0,0,0,0.5)'
+  },
   scrollViewStyle: {
     borderWidth: 1,
     paddingBottom: 20
   },
-  buttonViewStyle: {
-    height: 50,
+  searchInput:{
+    padding: 10,
+    borderColor: '#CCC',
+    borderWidth: 1
   }
 }
